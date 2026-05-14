107f87a7ff7639dd8a2bf89db5e996ff08f38ca4
kocherovaarina <arinakocerova@gmail.com>
2026-05-14 11:59:43 +0300

    Task done by Kocherova Arina from 25 IST 4-2
    Graphics will be added soon

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,69 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+Train::Train() : countOp(0), first(nullptr) {}
+
+void Train::addCar(bool light) {
+    Car* newCar = new Car{light, nullptr, nullptr};
+
+    if (!first) {
+        first = newCar;
+        first->next = first;
+        first->prev = first;
+    } else {
+        Car* last = first->prev;
+        last->next = newCar;
+        newCar->prev = last;
+        newCar->next = first;
+        first->prev = newCar;
+    }
+}
+
+int Train::getLength() {
+    if (!first) return 0;
+
+    int length = 1;
+    Car* current = first;
+    bool startLight = current->light;
+
+    current->light = false;
+    countOp++;
+
+    while (true) {
+        for (int i = 0; i < length; i++) {
+            current = current->next;
+            countOp++;
+        }
+
+        if (current->light) {
+            current->light = false;
+            countOp++;
+
+            for (int i = 0; i < length; i++) {
+                current = current->prev;
+                countOp++;
+            }
+
+            length++;
+        } else {
+            for (int i = 0; i < length; i++) {
+                current = current->prev;
+                countOp++;
+            }
+
+            if (!current->light) {
+                first->light = startLight;
+                if (startLight) {
+                    countOp++;
+                }
+                return length;
+            }
+
+            length++;
+        }
+    }
+}
+
+int Train::getOpCount() {
+    return countOp;
+}
