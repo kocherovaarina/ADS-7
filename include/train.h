b24569aa421fd768a5900a4997716a0f1624afd9
kocherovaarina <arinakocerova@gmail.com>
2026-05-14 14:58:40 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- include/train.h
+++ include/train.h
@@ -2,19 +2,71 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+struct Car {
+    bool light;
+    Car* next;
+    Car* prev;
+};
+
 class Train {
  private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
+    int countOp;
+    Car* first;
+
  public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+    Train() : countOp(0), first(nullptr) {}
+
+    void addCar(bool light) {
+        Car* newCar = new Car{light, nullptr, nullptr};
+        if (!first) {
+            first = newCar;
+            first->next = first;
+            first->prev = first;
+        } else {
+            Car* last = first->prev;
+            last->next = newCar;
+            newCar->prev = last;
+            newCar->next = first;
+            first->prev = newCar;
+        }
+    }
+
+    int getLength() {
+        if (!first) {
+            return 0;
+        }
+        if (first->next == first) {
+            return 1;
+        }
+
+        // Помечаем первый вагон
+        if (!first->light) {
+            first->light = true;
+            countOp++;
+        }
+
+        int length = 0;
+        Car* current = first;
+
+        while (true) {
+            current = current->next;
+            countOp++;
+            length++;
+
+            if (current->light) {
+                break;
+            }
+
+            current->light = true;
+            countOp++;
+        }
+
+        return length;
+    }
+
+    int getOpCount() {
+        return countOp;
+    }
 };
+
 #endif  // INCLUDE_TRAIN_H_
