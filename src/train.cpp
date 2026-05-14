3d569b6a5354846bf703ee948cbc5c1e349e4aee
kocherovaarina <arinakocerova@gmail.com>
2026-05-14 10:41:47 +0300

    Task done by Kocherova Arina from 25 IST 4-2
    Graphics will be added soon

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,55 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+
+int Train::getLength() {
+    if (!first) return 0;
+
+    int length = 0;
+    Car* current = first;
+    bool savedLight = current->light;
+
+    if (current->light) {
+        current->light = false;
+        countOp++;
+    }
+
+    while (true) {
+        for (int i = 0; i < length + 1; i++) {
+            current = current->next;
+            countOp++;
+        }
+
+        if (current->light) {
+            current->light = false;
+            countOp++;
+
+            for (int i = 0; i < length + 1; i++) {
+                current = current->prev;
+                countOp++;
+            }
+
+            length++;
+        } else {
+            for (int i = 0; i < length + 1; i++) {
+                current = current->prev;
+                countOp++;
+            }
+
+            if (!current->light) {
+                first->light = savedLight;
+                if (savedLight && !first->light) {
+                    countOp++;
+                } else if (!savedLight && first->light) {
+                    countOp++;
+                }
+                return length + 1;
+            }
+
+            length++;
+            for (int i = 0; i < length + 1; i++) {
+                current = current->next;
+                countOp++;
+            }
+        }
+    }
+}
