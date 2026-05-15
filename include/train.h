646fb4c715bf80ea7b6fe067d06f0befcb1b0ba0
kocherovaarina <arinakocerova@gmail.com>
2026-05-15 09:35:25 +0300

    Task done by Kocherova Arina from 25 IST 4-2

--- include/train.h
+++ include/train.h
@@ -2,19 +2,73 @@
 #ifndef INCLUDE_TRAIN_H_
 #define INCLUDE_TRAIN_H_
 
+struct Car {
+    bool light;
+    Car* next;
+    Car* prev;
+};
+
 class Train {
  private:
-  struct Car {
-    bool light; // состояние лампочки
-    Car *next;
-    Car *prev;
-  };
-  int countOp; // счетчик шагов (число переходов из вагона в вагон)
-  Car *first; // точка входа в поезд (первый вагон)
+    int countOp;
+    Car* first;
+
  public:
-  Train();
-  void addCar(bool light); // добавить вагон с начальным состоянием лампочки
-  int getLength();          // вычислить длину поезда
-  int getOpCount();         // вернуть число переходов (из вагона в вагон)
+    Train() : countOp(0), first(nullptr) {}
+
+    void addCar(bool light) {
+        Car* newCar = new Car{light, nullptr, nullptr};
+        if (!first) {
+            first = newCar;
+            first->next = first;
+            first->prev = first;
+        } else {
+            Car* last = first->prev;
+            last->next = newCar;
+            newCar->prev = last;
+            newCar->next = first;
+            first->prev = newCar;
+        }
+    }
+
+    int getLength() {
+        if (!first) return 0;
+        
+        // Шаг 1: выключаем свет ВО ВСЕХ вагонах
+        Car* current = first;
+        do {
+            if (current->light) {
+                current->light = false;
+                countOp++;
+            }
+            current = current->next;
+            countOp++;
+        } while (current != first);
+        
+        // Шаг 2: включаем свет в первом вагоне
+        first->light = true;
+        countOp++;
+        
+        // Шаг 3: считаем длину, двигаясь до горящего света
+        int length = 1;
+        current = first->next;
+        countOp++;
+        
+        while (current != first) {
+            if (current->light) {
+                break;
+            }
+            length++;
+            current = current->next;
+            countOp++;
+        }
+        
+        return length;
+    }
+
+    int getOpCount() {
+        return countOp;
+    }
 };
+
 #endif  // INCLUDE_TRAIN_H_
